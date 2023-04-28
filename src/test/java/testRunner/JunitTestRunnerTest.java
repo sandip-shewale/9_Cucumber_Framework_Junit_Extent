c8d4b901a95ae775381df85e44632d6862f8ff81
sandip.shewale <shewalesandeep001@gmail.com>
2023-04-28 21:04:28 +0400

    Selenium + Java + JUnit + Cucumber + Extent Behaviour Driven Development Framework

---
+++ src/test/java/testRunner/JunitTestRunnerTest.java
@@ -0,0 +1,20 @@
+package testRunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue = "stepDefinitions",
+        monochrome = true,
+        tags = "@PlaceOrder or @OffersPage",
+        plugin = {"html:target/cucumber.html",
+                "json:target/cucumber.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "rerun:target/failed_scenarios.txt"})
+
+public class JunitTestRunnerTest {
+
+}
---
+++ src/test/java/utils/TestBase.java
@@ -0,0 +1,42 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class TestBase {
+
+    public WebDriver driver;
+
+    public WebDriver WebDriverManager() throws IOException {
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/global.properties");
+        Properties prop = new Properties();
+        prop.load(fis);
+        String url = prop.getProperty("QAUrl");
+        String browser_properties = prop.getProperty("browser");
+        String browser_maven = System.getProperty("browser");
+
+        String browser = browser_maven != null ? browser_maven : browser_properties;
+
+        if (driver == null) {
+            if (browser.equalsIgnoreCase("chrome")) {
+                driver = new ChromeDriver();
+            }
+            if (browser.equalsIgnoreCase("firefox")) {
+                driver = new FirefoxDriver();
+            }
+            driver.manage().window().maximize();
+            driver.get(url);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        }
+
+        return driver;
+    }
+
+}
+
